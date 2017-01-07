b64b20a9b64a3b1eca05047211daa2cabf86c950
xucongli1989 <80213876@qq.com>
2017-01-07 09:52:56 +0800

    update XConfigGen.bat

--- XCLCMS.WebAPI/Config/XConfigGen.bat
+++ XCLCMS.WebAPI/Config/XConfigGen.bat
@@ -1,5 +1,10 @@
-:bat所在目录
-set XConfigGenBatPath=%~dp0
-:执行gulp命令
-node %XConfigGenPath% --xconfig  %XConfigGenBatPath%XConfigGen-Config.json --rootpath %XConfigGenBatPath%
-:pause
+%@Try%
+	:bat所在目录
+	set XConfigGenBatPath=%~dp0
+	:执行node命令
+	node %XConfigGenPath% --xconfig  %XConfigGenBatPath%XConfigGen-Config.json --rootpath %XConfigGenBatPath%
+	:pause
+%@EndTry%
+:@Catch
+  exit /b 0
+:@EndCatch
