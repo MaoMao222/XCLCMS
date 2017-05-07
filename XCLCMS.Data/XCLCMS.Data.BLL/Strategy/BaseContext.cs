73ea837717d833bde12caa771ec1afeee9d3e74e
xucongli1989 <80213876@qq.com>
2017-05-07 22:26:48 +0800

    update

--- XCLCMS.Data/XCLCMS.Data.BLL/Strategy/BaseContext.cs
+++ XCLCMS.Data/XCLCMS.Data.BLL/Strategy/BaseContext.cs
@@ -5,10 +5,22 @@
     /// </summary>
     public class BaseContext
     {
+        private XCLCMS.Data.Model.Custom.ContextModel _contextInfo = null;
+
         /// <summary>
         /// 当前上下文信息
         /// </summary>
-        public XCLCMS.Data.Model.Custom.ContextModel ContextInfo { get; set; }
+        public XCLCMS.Data.Model.Custom.ContextModel ContextInfo
+        {
+            get
+            {
+                return this._contextInfo ?? new Model.Custom.ContextModel();
+            }
+            set
+            {
+                this._contextInfo = value;
+            }
+        }
 
         /// <summary>
         /// 操作类型枚举
