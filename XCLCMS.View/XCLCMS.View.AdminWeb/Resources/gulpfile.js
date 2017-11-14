4c11646e6df68a7b2d9d45eb339407bb6ed757ec
xucongli1989 <80213876@qq.com>
2017-11-14 21:49:12 +0800

    压缩css

--- XCLCMS.View/XCLCMS.View.AdminWeb/Resources/gulpfile.js
+++ XCLCMS.View/XCLCMS.View.AdminWeb/Resources/gulpfile.js
@@ -2,6 +2,10 @@
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var sourcemaps = require('gulp-sourcemaps');
+var rebaseUrlPath = require('gulp-css-url-rebase');
+var modifyCssUrls = require('gulp-modify-css-urls');
+var concat = require('gulp-concat');
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('default', function (cb) {
     pump([
@@ -10,7 +14,42 @@ gulp.task('default', function (cb) {
         uglify(),
         sourcemaps.write('.'),
         gulp.dest('./dist/')
-    ],
-        cb
-    );
+    ], cb);
 });
+
+//登录页css
+gulp.task('min_logincss', (cb) => {
+    pump([
+        gulp.src([
+            './src/css/login.css'
+        ]),
+        rebaseUrlPath(),
+        modifyCssUrls({
+            prepend: '../'
+        }),
+        concat('login.css'),
+        uglifycss(),
+        gulp.dest('./dist/')
+    ], cb);
+});
+
+//主界面css
+gulp.task('min_maincss', (cb) => {
+    pump([
+        gulp.src([
+            './src/js/EasyUI/themes/icon.css',
+            './src/js/EasyUI/themes/default/easyui.css',
+            './src/js/artDialog/skins/blue.css',
+            './src/css/base.css'
+        ]),
+        rebaseUrlPath(),
+        modifyCssUrls({
+            prepend: '../'
+        }),
+        concat('style.css'),
+        uglifycss(),
+        gulp.dest('./dist/')
+    ], cb);
+});
+
+gulp.task('mincss', ['min_logincss', 'min_maincss']);
