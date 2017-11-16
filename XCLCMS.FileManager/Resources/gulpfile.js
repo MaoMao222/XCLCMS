37781c18ad69b9c5228aa23be3178b96ba29b14a
xucongli1989 <80213876@qq.com>
2017-11-16 20:40:29 +0800

    压缩文件管理系统中的css文件

--- XCLCMS.FileManager/Resources/gulpfile.js
+++ XCLCMS.FileManager/Resources/gulpfile.js
@@ -2,6 +2,10 @@
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var sourcemaps = require('gulp-sourcemaps');
+var rebaseUrlPath = require('gulp-css-url-rebase');
+var modifyCssUrls = require('gulp-modify-css-urls');
+var concat = require('gulp-concat');
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('default', function (cb) {
     pump([
@@ -13,4 +17,26 @@ gulp.task('default', function (cb) {
     ],
         cb
     );
+});
+
+//主界面css
+gulp.task('mincss', (cb) => {
+    pump([
+        gulp.src([
+            './src/css/Base.css',
+            './src/js/artDialog/skins/blue.css',
+            './src/js/bootstrap/css/bootstrap.min.css',
+            './src/js/Jcrop/css/jquery.Jcrop.css',
+            './src/js/plupload/jquery.plupload.queue/css/jquery.plupload.queue.css',
+            './src/js/EasyUI/themes/icon.css',
+            './src/js/EasyUI/themes/default/easyui.css'
+        ]),
+        rebaseUrlPath(),
+        modifyCssUrls({
+            prepend: '../'
+        }),
+        concat('style.css'),
+        uglifycss(),
+        gulp.dest('./dist/')
+    ], cb);
 });
